35a44fba36732daefbbcbabd9236dc787522d456
Tyger120 <101825946+Tyger120@users.noreply.github.com>
2023-12-04 16:34:25 -0600

    Add files via upload

---
+++ Email.php
@@ -0,0 +1,15 @@
+<?php	
+/*   
+ _________________________________________________
+| 						  |
+|    CONTACT ME NOW : https://t.me/ro0tM8n        |
+|                                                 |
+| Official Channel : https://t.me/ro0tM8nchannel  |
+|_________________________________________________|
+
+*/ 
+
+$email  = 'rootman@outlook.com'; // put your email here 
+
+
+?>
